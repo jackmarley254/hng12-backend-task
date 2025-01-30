d2d5889bc1e8d30c75e7b8fe0fb43f0e2ea59256
jackmarley254 <engndiritu1@gmail.com>
2025-01-30 13:35:16 +0300

    Fixing repository sync issue

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 @app.get("/")
 def get_info():
     return {
-        "email": "your-email@example.com",
+        "email": "jackndiritu97@gmail.com",
         "current_datetime": datetime.now(pytz.utc).isoformat(),
-        "github_url": "https://github.com/yourusername/hng12-backend-task"
+        "github_url": "https://github.com/jackmarley254/hng12-backend-task"
     }

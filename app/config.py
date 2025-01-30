666cae4e4a3304248e9ffe890f81699aecb05331
jackmarley254 <engndiritu1@gmail.com>
2025-01-30 13:46:46 +0300

    Update on Config file

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    EMAIL = os.getenv("EMAIL", "your-email@example.com")
-    GITHUB_URL = os.getenv("GITHUB_URL", "https://github.com/yourusername/hng12-backend-task")
+    EMAIL = os.getenv("EMAIL", "jackndiritu97@gmail.com")
+    GITHUB_URL = os.getenv("GITHUB_URL", "https://github.com/jackmarley254/hng12-backend-task")
